70ee333fc584a560fc0cbf19b546765d4341800a
Benjamin <obeejay99@gmail.com>
2025-01-09 08:20:01 +0100

    made change

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,6 +2,9 @@
 """
 
 """
+from os import getenv
+from sqlalchemy import create_engine
+
 
 class DBStorage:
     """
@@ -9,3 +12,16 @@ class DBStorage:
     """
     __engine = None
     __session = None
+
+    def __init__(self):
+        """
+        
+        """
+        username = getenv('HBNB_MYSQL_USER')
+        password = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db_name = getenv('HBNB_MYSQL_DB')
+
+        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(username, password, host, db_name)
+
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
